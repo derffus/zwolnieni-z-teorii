f5092d1fe879216dcab39fd7c86713d332127635
Krystian Kras <krystian.kras1qqaazzaaqq1@gmail.com>
2023-10-29 13:29:20 +0100

    zosia warzybok jako korepetytor

--- src/korepetytorzy.js
+++ src/korepetytorzy.js
@@ -16,6 +16,6 @@ function stworzKorepetytora(imie, nazwisko, miejscowosc, numerTelefonu, mail) {
   window[imie + nazwisko] = korepetytor;
   return window[imie + nazwisko];
 }
-let korepetytorzy = [stworzKorepetytora("Alicja","Kras","Piastów","123123123","alakras@gmail.com"), stworzKorepetytora("Krystian","Kras","Piastów","505505505","krysti@gmail.com"), stworzKorepetytora("Julia","Olak","Warszawa","420420420","julitajulita@gmail.com")];
+let korepetytorzy = [stworzKorepetytora("Alicja","Kras","Piastów","123123123","alakras@gmail.com"), stworzKorepetytora("Krystian","Kras","Piastów","505505505","krysti@gmail.com"), stworzKorepetytora("Julia","Olak","Warszawa","420420420","julitajulita@gmail.com"),stworzKorepetytora("Zosia","Warzybok","Gdańsk","123555999","hejkaTuZosia@wp.pl")];
 
 export default korepetytorzy;
