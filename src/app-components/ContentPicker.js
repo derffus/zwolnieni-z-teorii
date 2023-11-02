319c60599475408359acdaf36d11d4996ad3158c
Krystian Kras <krystian.kras1qqaazzaaqq1@gmail.com>
2023-11-02 20:30:31 +0100

    contentpickerlookingnice

--- src/app-components/ContentPicker.js
+++ src/app-components/ContentPicker.js
@@ -30,7 +30,9 @@ function ContentPicker(props) {
             props.updateState("klasa", event.target.value);
           }}
         >
-          <option value="" disabled selected>Klasa</option>
+          <option value="" disabled selected>
+            Klasa
+          </option>
           <option value="1">Klasa 1</option>
           <option value="2">Klasa 2</option>
           <option value="3">Klasa 3</option>
@@ -58,161 +60,132 @@ function ContentPicker(props) {
             props.updateState("temat", event.target.value);
           }}
         >
-          <option value="" disabled selected>Temat</option>
+          <option value="" disabled selected>
+            Temat
+          </option>
           <option value="jakistemat">jakistemat</option>
           <option value="innytemat">innytemat</option>
         </select>
       </div>
       <div id="dzial-select" className="content-picker-section">
-        {props.klasa === "1" && props.zakres === "podstawa" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="" disabled selected>Dzial</option>
-            <option value="1">I</option>
-            <option value="2">II</option>
-            <option value="3">III</option>
-            <option value="4">IV</option>
-            <option value="5">V</option>
-            <option value="6">VI</option>
-            <option value="7">VII</option>
-            <option value="8">VIII</option>
-          </select>
-        ) : props.klasa === "1" && props.zakres === "rozszerzenie" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">Zbiory liczbowe. Liczby rzeczywiste</option>
-            <option value="2">Wyrażenia algebraiczne</option>
-            <option value="3">Funkcja i jej własności</option>
-            <option value="4">Funkcja linowa</option>
-            <option value="5">
-              Układy równań liniowych z dwiema niewiadomymi
-            </option>
-            <option value="6">Podstawowe własności wybranych funkcji</option>
-            <option value="7">
-              Geometria płaska - pojęcia wstępne. Trójkąty
-            </option>
-            <option value="8">Trygonometria kąta ostrego</option>
-          </select>
-        ) : props.klasa === "2" && props.zakres === "podstawa" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">Przekształcenia wykresów funkcji</option>
-            <option value="2">
-              Równania i nierówności z wartością bezwzględną
-            </option>
-            <option value="3">Funkcja kwadratowa</option>
-            <option value="4">Geometria płaska - okręgi i koła</option>
-            <option value="5">Trygonometria</option>
-            <option value="6">Geometria analityczna</option>
-            <option value="7">
-              Geometria płaska - rozwiązywanie trójkątów
-            </option>
-            <option value="8">Wielomiany</option>
-          </select>
-        ) : props.klasa === "2" && props.zakres === "rozszerzenie" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">Przekształcenia wykresów funkcji</option>
-            <option value="2">
-              Równania i nierówności z wartością bezwzględną
-            </option>
-            <option value="3">Funkcja kwadratowa</option>
-            <option value="4">Geometria płaska - okręgi i koła</option>
-            <option value="5">Trygonometria</option>
-            <option value="6">Geometria analityczna</option>
-            <option value="7">
-              Geometria płaska - rozwiązywanie trójkątów
-            </option>
-            <option value="8">Wielomiany</option>
-          </select>
-        ) : props.klasa === "3" && props.zakres === "podstawa" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">I</option>
-            <option value="2">II</option>
-            <option value="3">III</option>
-            <option value="4">IV</option>
-            <option value="5">V</option>
-            <option value="6">VI</option>
-            <option value="7">VII</option>
-            <option value="8">VIII</option>
-          </select>
-        ) : props.klasa === "3" && props.zakres === "rozszerzenie" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">Ułamki algebraiczne</option>
-            <option value="2">Ciągi</option>
-            <option value="3">Kombinatoryka</option>
-            <option value="4">Geometria płaska - czworokąty</option>
-            <option value="5">Geometria płaska - pole czworokąta</option>
-            <option value="6">Elementaż analizy matematycznej</option>
-            <option value="7">Trygonometria</option>
-            <option value="8">Geometria analityczna</option>
-          </select>
-        ) : props.klasa === "4" && props.zakres === "podstawa" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">I</option>
-            <option value="2">II</option>
-            <option value="3">III</option>
-            <option value="4">IV</option>
-            <option value="5">V</option>
-            <option value="6">VI</option>
-            <option value="7">VII</option>
-            <option value="8">VIII</option>
-          </select>
-        ) : props.klasa === "4" && props.zakres === "rozszerzenie" ? (
-          <select
-            className="content-picker-select"
-            onChange={(event) => {
-              props.updateState("dzial", event.target.value);
-            }}
-          >
-            <option value="1">Funkcja wykładnicza</option>
-            <option value="2">Funkcja logarytmiczna</option>
-            <option value="3">Elementy statystyki</option>
-            <option value="4">Rachunek prawdopodobieństwa</option>
-            <option value="5">Geometria przestrzenna. Wielościany</option>
-            <option value="6">Geometria przestrzenna. Bryły obrotowe</option>
-          </select>
-        ) : (
-          ""
-        )}
+        <select
+          className="content-picker-select"
+          onChange={(event) => {
+            props.updateState("dzial", event.target.value);
+          }}
+        >
+          {props.klasa === "1" && props.zakres === "podstawa" ? (
+            <>
+              <option value="" disabled selected>
+                Dzial
+              </option>
+              <option value="1">I</option>
+              <option value="2">II</option>
+              <option value="3">III</option>
+              <option value="4">IV</option>
+              <option value="5">V</option>
+              <option value="6">VI</option>
+              <option value="7">VII</option>
+              <option value="8">VIII</option>
+            </>
+          ) : props.klasa === "1" && props.zakres === "rozszerzenie" ? (
+            <>
+              <option value="1">Zbiory liczbowe. Liczby rzeczywiste</option>
+              <option value="2">Wyrażenia algebraiczne</option>
+              <option value="3">Funkcja i jej własności</option>
+              <option value="4">Funkcja linowa</option>
+              <option value="5">
+                Układy równań liniowych z dwiema niewiadomymi
+              </option>
+              <option value="6">Podstawowe własności wybranych funkcji</option>
+              <option value="7">
+                Geometria płaska - pojęcia wstępne. Trójkąty
+              </option>
+              <option value="8">Trygonometria kąta ostrego</option>
+            </>
+          ) : props.klasa === "2" && props.zakres === "podstawa" ? (
+            <>
+              <option value="1">Przekształcenia wykresów funkcji</option>
+              <option value="2">
+                Równania i nierówności z wartością bezwzględną
+              </option>
+              <option value="3">Funkcja kwadratowa</option>
+              <option value="4">Geometria płaska - okręgi i koła</option>
+              <option value="5">Trygonometria</option>
+              <option value="6">Geometria analityczna</option>
+              <option value="7">
+                Geometria płaska - rozwiązywanie trójkątów
+              </option>
+              <option value="8">Wielomiany</option>
+            </>
+          ) : props.klasa === "2" && props.zakres === "rozszerzenie" ? (
+            <>
+              <option value="1">Przekształcenia wykresów funkcji</option>
+              <option value="2">
+                Równania i nierówności z wartością bezwzględną
+              </option>
+              <option value="3">Funkcja kwadratowa</option>
+              <option value="4">Geometria płaska - okręgi i koła</option>
+              <option value="5">Trygonometria</option>
+              <option value="6">Geometria analityczna</option>
+              <option value="7">
+                Geometria płaska - rozwiązywanie trójkątów
+              </option>
+              <option value="8">Wielomiany</option>
+            </>
+          ) : props.klasa === "3" && props.zakres === "podstawa" ? (
+            <>
+              <option value="1">I</option>
+              <option value="2">II</option>
+              <option value="3">III</option>
+              <option value="4">IV</option>
+              <option value="5">V</option>
+              <option value="6">VI</option>
+              <option value="7">VII</option>
+              <option value="8">VIII</option>
+            </>
+          ) : props.klasa === "3" && props.zakres === "rozszerzenie" ? (
+            <>
+              <option value="1">Ułamki algebraiczne</option>
+              <option value="2">Ciągi</option>
+              <option value="3">Kombinatoryka</option>
+              <option value="4">Geometria płaska - czworokąty</option>
+              <option value="5">Geometria płaska - pole czworokąta</option>
+              <option value="6">Elementaż analizy matematycznej</option>
+              <option value="7">Trygonometria</option>
+              <option value="8">Geometria analityczna</option>
+            </>
+          ) : props.klasa === "4" && props.zakres === "podstawa" ? (
+            <>
+              <option value="1">I</option>
+              <option value="2">II</option>
+              <option value="3">III</option>
+              <option value="4">IV</option>
+              <option value="5">V</option>
+              <option value="6">VI</option>
+              <option value="7">VII</option>
+              <option value="8">VIII</option>
+            </>
+          ) : props.klasa === "4" && props.zakres === "rozszerzenie" ? (
+            <>
+              <option value="1">Funkcja wykładnicza</option>
+              <option value="2">Funkcja logarytmiczna</option>
+              <option value="3">Elementy statystyki</option>
+              <option value="4">Rachunek prawdopodobieństwa</option>
+              <option value="5">Geometria przestrzenna. Wielościany</option>
+              <option value="6">Geometria przestrzenna. Bryły obrotowe</option>
+            </>
+          ) : null}
+        </select>
       </div>
       <div id="zrodlo-select" className="content-picker-section">
         <select
           className="content-picker-select"
           onChange={(event) => props.updateState("zrodlo", event.target.value)}
         >
-          <option value="" disabled selected>Źródło</option>
+          <option value="" disabled selected>
+            Źródło
+          </option>
           <option value="youtube">Youtube</option>
           <option value="artykul">Artykuł</option>
         </select>
