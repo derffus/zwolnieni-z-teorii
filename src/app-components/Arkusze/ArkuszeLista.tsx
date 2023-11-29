651ad49496cea32c8a8b5b5fbe7f0952b530e8fe
Krystian Kras <krystian.kras1qqaazzaaqq1@gmail.com>
2023-11-29 20:47:06 +0100

    pdf dziala

--- src/app-components/Arkusze/ArkuszeLista.tsx
+++ src/app-components/Arkusze/ArkuszeLista.tsx
@@ -1,44 +1,38 @@
 import React from "react";
 import "../../component-styles/Arkusze.scss";
-
 function ArkuszeLista(props) {
-  const downloadPDF = (fileName:string) => {
-    /*const pdfPath = `./ArkuszePDF/${fileName}.pdf`;
-    const link = document.createElement("a");
-    link.href = pdfPath;
-    link.download = `${fileName}.pdf`;
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);*/
-    fetch(`./ArkuszePDF/${fileName}.pdf`).then((response) => {
-      response.blob().then((blob) => {
-        const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement("a");
-        alink.href = fileURL;
-        alink.download = `${fileName}.pdf`;
-        alink.click();
-      });
-    });
+  const openPDF = (fileName: string) => {
+    window.open(require(`./ArkuszePDF/${fileName}.pdf`), "_blank");
   };
   return (
     <div id="arkusze-lista">
       <div className="lista">
-        <button className="lista-button">
-          Matura podstawowa maj 2023 formuła 2015
-        </button>
-        <button
-          className="lista-button"
-          onClick={() => {
-            downloadPDF("MaturaPodstawowaMaj2023Formula2015Odpowiedzi");
-          }}
-        >
-          Matura podstawowa maj 2023 formuła 2015 - odpowiedzi
-        </button>
-        <button className="lista-button">AA</button>
-        <button className="lista-button">HH</button>
-        <button className="lista-button">SHHH</button>
-        <button className="lista-button">AHAHA</button>
+        <div className="lista-div maturaPodstawowaMaj2023Formula2015">
+          <button
+            className="lista-button"
+            onClick={() => {
+              openPDF("MaturaPodstawowaMaj2023Formula2015");
+            }}
+          >
+            Matura podstawowa maj 2023 formuła 2015
+          </button>
+          <button
+            className="lista-button"
+            onClick={() => {
+              openPDF("MaturaPodstawowaMaj2023Formula2015Odpowiedzi");
+            }}
+          >
+            Matura podstawowa maj 2023 formuła 2015 - odpowiedzi
+          </button>
+        </div>
+        <div className="lista-div">
+          <button className="lista-button">Matura x</button>
+          <button className="lista-button">Matura x odpowiedzi</button>
+        </div>
+        <div className="lista-div">
+          <button className="lista-button">Matura y</button>
+          <button className="lista-button">Matura y odpowiedzi</button>
+        </div>
       </div>
     </div>
   );
